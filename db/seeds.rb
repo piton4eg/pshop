0e16577e14625447cc3a6ec41c0a1e4082088cab
Markov Alexey <piton4eg@mail.ru>
2013-08-20 23:01:37 +0400

    start read AWD book

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+
+Product.create(name: 'Programming Ruby',
+               description: 
+               %{<p> Ruby is .... </p>},
+               image_url: '1.jpg',
+               price: 100)
+
+Product.create(name: '2nd Programming Ruby',
+               description: 
+               %{<p> Ruby is ddd .... </p>},
+               image_url: '2.jpg',
+               price: 100)
+
+Product.create(name: 'Ruby Language',
+               description: 
+               %{<p> It's Ruby .... </p>},
+               image_url: '3.png',
+               price: 100)
+
+Product.create(name: 'Programming Ruby',
+               description: 
+               %{<p> Ruby is .... </p>},
+               image_url: '4.jpg',
+               price: 100)
