c1447bf7d98291d5297181e462919902a9d1fe19
Markov Alexey <piton4eg@mail.ru>
2013-08-21 23:57:09 +0400

    added cart to right part (11.1)

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -29,7 +29,7 @@ class LineItemsController < ApplicationController
     @line_item = @cart.add_product(product.id)
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to root_url }
         format.json { render action: 'show', status: :created, location: @line_item }
       else
         format.html { render action: 'new' }
