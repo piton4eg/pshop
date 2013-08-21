68000ce622df68d6068c82afa6fdf1e2a54fd1ac
Markov Alexey <piton4eg@mail.ru>
2013-08-21 22:53:11 +0400

    edit combine cart and add empty cart action (ch.10)

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,5 @@
 class CartsController < ApplicationController
-  before_action :set_cart, only: [:show, :edit, :update, :destroy]
+  before_action :set_cart, only: [:edit, :update]
 
   # GET /carts
   # GET /carts.json
@@ -10,6 +10,17 @@ class CartsController < ApplicationController
   # GET /carts/1
   # GET /carts/1.json
   def show
+    begin
+      @cart = Cart.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      logger.error "Попытка доступа к несуществующей корзине #{params[:id]}"
+      redirect_to root_url, notice: "Корзина не существует"
+    else
+      respond_to do |format|
+        format.html
+        format.json { render json: @cart }
+      end
+    end
   end
 
   # GET /carts/new
@@ -54,10 +65,12 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
+    @cart = current_cart
     @cart.destroy
+    session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url }
-      format.json { head :no_content }
+      format.html { redirect_to root_url, notice: "Ваша корзина пуста" }
+      format.json { head :ok }
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130820201047) do
+ActiveRecord::Schema.define(version: 20130821180508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20130820201047) do
     t.integer  "cart_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "quantity",   default: 1
   end
 
   create_table "products", force: true do |t|
